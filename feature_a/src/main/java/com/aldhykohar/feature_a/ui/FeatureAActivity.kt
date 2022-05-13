d56590418241780cd44dccced516eff054777b6d
aldhykohar <aldhykohar@stimednp.ac.id>
2022-05-13 16:21:57 +0800

    :zap: intent to other activity from other module not have relation

--- feature_a/src/main/java/com/aldhykohar/feature_a/ui/FeatureAActivity.kt
+++ feature_a/src/main/java/com/aldhykohar/feature_a/ui/FeatureAActivity.kt
@@ -1,12 +1,35 @@
 package com.aldhykohar.feature_a.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aldhykohar.feature_a.R
+import com.aldhykohar.feature_a.databinding.ActivityFeatureABinding
 
 class FeatureAActivity : AppCompatActivity() {
+
+    private val binding: ActivityFeatureABinding by lazy {
+        ActivityFeatureABinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feature_a)
+        setContentView(binding.root)
+        initClick()
+    }
+
+    private fun initClick() {
+        binding.moduleBMB.setOnClickListener {
+            val intent: Intent
+            try {
+                intent = Intent(
+                    this,
+                    Class.forName("com.aldhykohar.feature_b.ui.FeatureBActivity")
+                )
+                startActivity(intent)
+            } catch (e: ClassNotFoundException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
